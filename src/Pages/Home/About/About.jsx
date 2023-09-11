9f02333a97c244c40cf9de0e9c63f126e7ca74cf
OliUllah1 <oliullah915@gmail.com>
2023-09-11 13:32:31 +0600

    ranking section done

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -11,8 +11,8 @@ import { FaCircle } from "react-icons/fa";
 
 const About = () => {
     return (
-            <div className='bg-[#237c3a] px-10 flex flex-col lg:flex-row lg:px-40 lg:py-28 py-16 relative'>
-                <img className='absolute -bottom-20 left-[44%] w-40' src={stamp} alt="" />
+            <div className='bg-[#237c3a] px-10 flex flex-col lg:flex-row lg:px-40 lg:py-28 py-24 relative'>
+                <img className='absolute -bottom-12 left-40 lg:-bottom-20 lg:left-[44%] w-24 lg:w-40' src={stamp} alt="" />
                 <div className='w-full lg:px-5 order-2 lg:order-1'>
                     <h1 className='px-2 ml-3 lg:ml-10 py-2 lg:py-[10px] shadow-md rounded bg-white w-[90px] lg:w-24 flex items-center gap-2 text-gray-500 text-xs lg:text-sm font-bold'>Your List <FaHeart className='text-[#237c3a]'></FaHeart></h1>
                     <div className=' ml-10 lg:ml-20 my-7'>
@@ -35,9 +35,9 @@ const About = () => {
                         
                     </div>
                     </div>
-                    <div className='mb-7 mr-7 lg:mr-0'>
+                    <div className='mb-7 mr-7 lg:mr-14'>
                     <div className='bg-white lg:w-[80%] flex items-center  lg:px-3 px-2 py-2 rounded-md gap-3'>
-                        <div className='flex lg:w-[280px] items-center gap-2 pr-10 border-r-2'>
+                        <div className='flex lg:w-[255px] items-center gap-2 pr-10 border-r-2'>
                             <img className='lg:w-14 w-12 rounded-md' src={logo2} alt="" />
                             <div>
                                 <div className='flex items-center gap-1'>
@@ -60,7 +60,7 @@ const About = () => {
                     </div>
                     <div className='lg:ml-20 ml-14'>
                     <div className='bg-white lg:w-[80%] flex items-center  lg:px-3 px-2 py-2 rounded-md gap-3'>
-                        <div className='flex w-[360px] items-center gap-2 pr-10 border-r-2'>
+                        <div className='flex lg:w-[340px] items-center gap-2 pr-10 border-r-2'>
                             <img className='lg:w-14 w-12 rounded-md' src={logo3} alt="" />
                             <div>
                                 <div className='flex items-center gap-1'>
--- src/Pages/Home/Information/Information.jsx
+++ src/Pages/Home/Information/Information.jsx
@@ -5,57 +5,58 @@ import logo2 from '../../../assets/images/about/about-logo-3.webp'
 import { FaCircle,FaDollarSign } from "react-icons/fa";
 const Information = () => {
     return (
-        <div className='flex'>
+        <div className='lg:flex'>
             <div className='w-[100%] bg-white flex items-center justify-center'> 
-            <div className='p-20'>
-            <p className='text-[#237c3a] font-medium text-xl mb-5'>CHOOSE YOUR SCHOOL</p>
-            <h1 className='text-[#237c3a] font-bold text-3xl'>Everything you need to <br className='hidden lg:block' /> make your decision.</h1>
+            <div className='px-10 lg:px-20 py-20'>
+            <p className='text-[#237c3a] font-medium mb-5 tracking-[2px]'>CHOOSE YOUR SCHOOL</p>
+            <h1 className='text-[#237c3a] font-bold text-[22px] lg:text-3xl'>Everything you need to <br className='hidden lg:block' /> make your decision.</h1>
             <div className='stroke-[#237c3a] my-5 stroke-[4] w-40'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 186 17" fill="none"><path d="M184.68 8.95006C169.95 1.68006 152.06 1.68006 137.32 8.95006C124.03 15.5101 107.9 15.5101 94.61 8.95006C79.88 1.68006 61.99 1.68006 47.25 8.95006C33.96 15.5101 17.83 15.5101 4.54 8.95006L1 7.43006"></path></svg></div>
-            <p className='text-[#333] text-lg '>With the information you care about on every <br className='hidden lg:block' /> school in America, we make it easy for you to <br className='hidden lg:block' /> compare schools and colleges to make your <br className='hidden lg:block' /> decision.</p>
-            <button className='mt-7 text-[#237c3a] font-bold text-lg underline mb-4'>Compare K-12 Schools</button> <br />
+            <p className='text-[#333] lg:text-lg '>With the information you care about on every <br className='hidden lg:block' /> school in America, we make it easy for you to <br className='hidden lg:block' /> compare schools and colleges to make your <br className='hidden lg:block' /> decision.</p>
+            <button className='mt-7 text-[#237c3a] font-bold text-lg underline mb-3'>Compare K-12 Schools</button> <br />
             <button className='text-[#237c3a] font-bold text-lg underline'>Compare Colleges</button>
             </div>
             </div>
-            <div className='w-[100%] bg-[#ff9800] flex justify-center'>
-                <div className='p-20 flex gap-10'>
-                    <div className='w-48 bg-white rounded-xl'>
-                        <img className='w-full h-24 rounded-t-lg' src={logo2} alt="" />
-                        <div className='p-5'>
-                        <FaCircle className='mb-1 mt-2 text-4xl text-[#237c3a]'></FaCircle>
-                        <h3 className='font-bold text-2xl'>UCLA</h3>
-                        <div className='mt-5 space-y-4'>
+            <div className='w-full bg-[#ff9800] flex justify-center'>
+                <div className='p-20 flex gap-7 lg:gap-10'>
+                    <div className='w-32 lg:w-48 bg-white rounded-xl'>
+                        <img className='w-full h-20 lg:h-24 rounded-t-lg' src={logo2} alt="" />
+                        <div className='lg:p-5 p-3'>
+                        <FaCircle className='mb-1 mt-2 text-2xl lg:text-4xl text-[#237c3a]'></FaCircle>
+                        <h3 className='font-bold text-xs lg:text-2xl'>UCLA</h3>
+                        <div className='lg:mt-5 mt-2 space-y-2 lg:space-y-4'>
                             <div>
-                                <p className='text-[#102f25] font-medium'>Acceptance Rate</p>
-                                <h5 className='text-[#237c3a] font-semibold text-2xl'>16%</h5>
+                                <p className='text-[#102f25] font-bold lg:font-medium lg:text-base text-[9px]'>Acceptance Rate</p>
+                                <h5 className='text-[#237c3a] font-semibold lg:text-2xl text-lg'>16%</h5>
                             </div>
                             <div>
-                                <p className='text-[#102f25] font-medium'>Net Price</p>
-                                <h5 className='flex items-center text-[#237c3a] font-semibold text-2xl'><FaDollarSign></FaDollarSign> 18,522</h5>
+                                <p className='text-[#102f25] font-bold lg:font-medium lg:text-base text-[9px]'>Net Price</p>
+                                <h5 className='flex items-center text-[#237c3a] font-semibold text-sm lg:text-2xl'><FaDollarSign></FaDollarSign> 18,522</h5>
                             </div>
                             <div>
-                                <p className='text-[#102f25] font-medium'>SAT Range</p>
-                                <h5 className='text-[#237c3a] font-semibold text-2xl'>1300-1530</h5>
+                                <p className='text-[#102f25] font-bold lg:font-medium lg:text-base text-[9px]'>SAT Range</p>
+                                <h5 className='text-[#237c3a] font-semibold lg:text-2xl text-sm'>1300-1530</h5>
                             </div>
                         </div>
                         </div>
                     </div>
-                    <div className='w-48 bg-white rounded-xl'>
-                        <img className='w-full h-24 rounded-t-lg' src={logo1} alt="" />
-                        <div className='p-5'>
-                        <FaCircle className='mb-1 mt-2 text-4xl text-[#237c3a]'></FaCircle>
-                        <h3 className='font-bold text-2xl'>UC - Berkeley</h3>
-                        <div className='mt-5 space-y-4'>
+                    <div className=' w-32 lg:w-48 bg-white rounded-xl'>
+                        <img className='w-full h-20 lg:h-24 rounded-t-lg' src={logo1} alt="" />
+                        <div className='lg:p-5 p-3'>
+                        <FaCircle className='mb-1 mt-2 text-2xl lg:text-4xl text-[#237c3a]'></FaCircle>
+                        <h3 className='font-bold text-xs lg:text-2xl'>UC - Berkeley</h3>
+                        <div className='lg:mt-5 mt-2 space-y-2 lg:space-y-4'>
                             <div>
-                                <p className='text-[#102f25] font-medium'>Acceptance Rate</p>
-                                <h5 className='text-[#237c3a] font-semibold text-2xl'>16%</h5>
+                                <p className='text-[#102f25] font-bold lg:font-medium lg:text-base text-[9px]'>Acceptance Rate</p>
+
+                                <h5 className='text-[#237c3a] font-semibold lg:text-2xl text-lg'>16%</h5>
                             </div>
                             <div>
-                                <p className='text-[#102f25] font-medium'>Net Price</p>
-                                <h5 className='flex items-center text-[#237c3a] font-semibold text-2xl'><FaDollarSign></FaDollarSign> 18,522</h5>
+                                <p className='text-[#102f25] font-bold lg:font-medium lg:text-base text-[9px]'>Net Price</p>
+                                <h5 className='flex items-center text-[#237c3a] font-semibold text-sm lg:text-2xl'><FaDollarSign></FaDollarSign> 18,522</h5>
                             </div>
                             <div>
-                                <p className='text-[#102f25] font-medium'>SAT Range</p>
-                                <h5 className='text-[#237c3a] font-semibold text-2xl'>1300-1530</h5>
+                                <p className='text-[#102f25] font-bold lg:font-medium lg:text-base text-[9px]'>SAT Range</p>
+                                <h5 className='text-[#237c3a] font-semibold lg:text-2xl text-sm'>1300-1530</h5>
                             </div>
                         </div>
                         </div>
